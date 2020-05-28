500933fed1f571660e4c29020ba3617d7ecfefbc
阿部直也 <n.abe@gemcook.com>
2020-05-28 11:46:11 +0900

    Modified CREATE_TODO actions

--- src/redux/reducers/todos/index.js
+++ src/redux/reducers/todos/index.js
@@ -4,7 +4,7 @@ import { CREATE_TODO, FETCH_TODOS } from "../../actions/todos/types";
 export default (state = {}, action) => {
   switch (action.type) {
     case CREATE_TODO:
-      return { ...state, [action.payload.id]: action.payload };
+      return { ...state };
     case FETCH_TODOS:
       return { ...state, ...action.payload };
     default:
